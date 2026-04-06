1538e20a18c849c7824af48bb963627613619aee
Basha2306 <134443035+basha2123@users.noreply.github.com>
2026-04-06 12:14:03 +0530

    Create reverseOfString.cpp

---
+++ reverseOfString.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void reverse(string &s, int l, int r){
+    if(l >= r) return;
+
+    swap(s[l], s[r]);
+    reverse(s, l + 1, r - 1);
+}
+
+int main(){
+    string s = "HELLO";
+    reverse(s, 0, s.length() - 1);
+    cout << s;
+    return 0;
+}
