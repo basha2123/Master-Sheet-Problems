a1cb9141724f0b6650991a60373e9d9bad97f395
Basha2306 <134443035+basha2123@users.noreply.github.com>
2026-04-08 21:03:09 +0530

    Create countOfDivisors.cpp

---
+++ countOfDivisors.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int count = 0;
+    
+    for(int i=1; i<=n/2; i++){
+        if(n%i == 0) count++;
+    }
+    cout << count;
+    return 0;
+}
